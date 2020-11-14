65a451650453eb3530d5eee62ab38e1f33c2d015
David Dias <daviddias.p@gmail.com>
2020-11-14 23:01:54 +0000

    chore: remove node_modules, standard lint everything

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,28 +1,28 @@
-var fs = require('fs');
-var srt = require('./../../index.js');
+var fs = require('fs')
+var srt = require('./../../index.js')
 
 /* we use the Png module to export the generated image to Png */
-var Png = require('png').Png;
+var Png = require('png').Png
 
 /* parse the 'world' that is going to be ray traced */
-var SCENE_PATH = './example-scenes/pokeball.rt';
-var scene = srt.prepareScene.byPath(SCENE_PATH);
+var SCENE_PATH = './example-scenes/pokeball.rt'
+var scene = srt.prepareScene.byPath(SCENE_PATH)
 
 /* number of units per split (total: N_UNITS * N_UNITS) */
-var N_UNITS = 50;
+var N_UNITS = 50
 
 /* create each individual task to be executed */
 var tasks = srt.prepareTasks({
-  split: N_UNITS, 
+  split: N_UNITS,
   width: scene.global.width,
   height: scene.global.height
-});
+})
 
 /* buffer that will glue the image all together before being exported */
-var rgb = new Buffer(scene.global.width * scene.global.height * 3);
+var rgb = new Buffer(scene.global.width * scene.global.height * 3)
 
 /* take each task and execute a ray trace on the world with it */
-var results = tasks.map(function(task) {
+var results = tasks.map(function (task) {
   return {
     begin_x: task.begin_x,
     end_x: task.end_x,
@@ -30,25 +30,22 @@ var results = tasks.map(function(task) {
     end_y: task.end_y,
     animation: task.animation,
     data: srt.runTask(scene, task).data
-  };
-});
+  }
+})
 
 /* glue each ray trace result back together */
 results.map(function (el) {
-  var i = 0;
-  for(var y = el.begin_y; y < el.end_y; y++) {
-    for(var x = el.begin_x; x < el.end_x; x++) {
-      var z = (x * scene.global.width + y) * 3;
-      rgb[z] = el.data[i++];
-      rgb[z+1] = el.data[i++];
-      rgb[z+2] = el.data[i++];
+  var i = 0
+  for (var y = el.begin_y; y < el.end_y; y++) {
+    for (var x = el.begin_x; x < el.end_x; x++) {
+      var z = (x * scene.global.width + y) * 3
+      rgb[z] = el.data[i++]
+      rgb[z + 1] = el.data[i++]
+      rgb[z + 2] = el.data[i++]
     }
   }
-});
-
+})
 
 /* write the image to a png */
-var png = new Png(rgb, scene.global.width, scene.global.height, 'rgb');
-fs.writeFileSync('/tmp/out.png', png.encodeSync());
-
-
+var png = new Png(rgb, scene.global.width, scene.global.height, 'rgb')
+fs.writeFileSync('/tmp/out.png', png.encodeSync())
--- src/lib/prepare-tasks.js
+++ src/lib/prepare-tasks.js
@@ -1,36 +1,35 @@
-var fs = require('fs');
+var fs = require('fs')
 
-module.exports = prepare;
+module.exports = prepare
 
-function prepare(opts) {
-
-  var split = opts.split || 10;
-  var animation = opts.animation || false;
-  var data = [];
+function prepare (opts) {
+  var split = opts.split || 10
+  var animation = opts.animation || false
+  var data = []
 
   /* Calculate jobs sizes */
-  var jobWidth = Math.floor(opts.width / split);
-  var splitWidth = Math.ceil(opts.width / jobWidth);
+  var jobWidth = Math.floor(opts.width / split)
+  var splitWidth = Math.ceil(opts.width / jobWidth)
 
-  var jobHeight = Math.floor(opts.height / split);
-  var splitHeight = Math.ceil(opts.height / jobHeight);
+  var jobHeight = Math.floor(opts.height / split)
+  var splitHeight = Math.ceil(opts.height / jobHeight)
 
-  var id = 0;
+  var id = 0
   if (animation) {
     for (var frame = 0; frame < animation.frames; frame++) {
       for (var i = 0; i < splitWidth; i++) {
         for (var j = 0; j < splitHeight; j++) {
           data.push({
-            'id': id++,
-            'animation': {
-              'frame': frame,
-              'frames': animation.frames
+            id: id++,
+            animation: {
+              frame: frame,
+              frames: animation.frames
             },
-            'begin_x': jobHeight * j,
-            'end_x': j < splitHeight - 1 ? jobHeight * (j + 1) : opts.height,
-            'begin_y': jobWidth * i,
-            'end_y': i < splitWidth - 1 ? jobWidth * (i + 1) : opts.width
-          });
+            begin_x: jobHeight * j,
+            end_x: j < splitHeight - 1 ? jobHeight * (j + 1) : opts.height,
+            begin_y: jobWidth * i,
+            end_y: i < splitWidth - 1 ? jobWidth * (i + 1) : opts.width
+          })
         }
       }
     }
@@ -38,14 +37,14 @@ function prepare(opts) {
     for (var k = 0; k < splitWidth; k++) {
       for (var z = 0; z < splitHeight; z++) {
         data.push({
-          'id': id++,
-          'begin_x': jobHeight * z,
-          'end_x': z < splitHeight - 1 ? jobHeight * (z + 1) : opts.height,
-          'begin_y': jobWidth * k,
-          'end_y': k < splitWidth - 1 ? jobWidth * (k + 1) : opts.width
-        });
+          id: id++,
+          begin_x: jobHeight * z,
+          end_x: z < splitHeight - 1 ? jobHeight * (z + 1) : opts.height,
+          begin_y: jobWidth * k,
+          end_y: k < splitWidth - 1 ? jobWidth * (k + 1) : opts.width
+        })
       }
     }
   }
-  return data;
+  return data
 }
