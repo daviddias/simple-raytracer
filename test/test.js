ee4b3171ea60e1e5492b30dd11e54a647b44ba94
David Dias <daviddias.p@gmail.com>
2020-11-15 09:37:28 +0000

    test: migrate test to tape

--- test/test.js
+++ test/test.js
@@ -1,94 +1,79 @@
-const Lab = require('lab')
-const Code = require('code')
-const lab = exports.lab = Lab.script()
-
-const experiment = lab.experiment
-const test = lab.test
-const before = lab.before
-const after = lab.after
-const expect = Code.expect
-
+const test = require('tape')
 const fs = require('fs')
-const srt = require('./../src/index.js')
 const PNG = require('pngjs').PNG
+const srt = require('./../src')
 
-experiment('local: ', function () {
-  // Constants
-  const N_UNITS = 10
-  const SCENE_PATH = './example-scenes/pokeball.rt'
+// Constants
+const N_UNITS = 10
+const SCENE_PATH = './scenes/pokeball.rt'
 
-  let scene
-  let tasks
-  let results
+let scene
+let tasks
+let results
 
-  before(function (done) {
-    done()
+// Note for the reader. These tests don't really test anything other than making sure that the func doesn't explore
+// TODO for the future, improve tests
+
+test('prepare scene by path', function (t) {
+  scene = srt.prepareScene.byPath(SCENE_PATH)
+  t.end()
+})
+
+test('prepare scene by file', function (t) {
+  t.end()
+})
+
+test('compare two equal scenes', function (t) {
+  // TODO
+  t.end()
+})
+
+test('compare two different scenes', function (t) {
+  // TODO
+  t.end()
+})
+
+test('prepare tasks', function (t) {
+  tasks = srt.prepareTasks({
+    split: N_UNITS, /* Number of tasks the job is going to be divided into */
+    width: scene.global.width,
+    height: scene.global.height
   })
+  t.end()
+})
 
-  after(function (done) {
-    done()
+test('ray trace', function (t) {
+  results = tasks.map(function (task) {
+    return {
+      begin_x: task.begin_x, // jscs:disable
+      end_x: task.end_x,
+      begin_y: task.begin_y,
+      end_y: task.end_y,
+      animation: task.animation,
+      data: srt.runTask(scene, task).data // jscs:enable
+    }
   })
+  t.end()
+})
 
-  test('prepare scene by path', function (done) {
-    scene = srt.prepareScene.byPath(SCENE_PATH)
-    done()
-  })
+test('produce output in png', function (t) {
+  const rgb = Buffer.alloc(scene.global.width * scene.global.height * 3)
 
-  test('prepare scene by file', function (done) {
-    done()
-  })
-
-  test('compare two equal scenes', function (done) {
-    // TODO
-    done()
-  })
-
-  test('compare two different scenes', function (done) {
-    // TODO
-    done()
-  })
-
-  test('prepare tasks', function (done) {
-    tasks = srt.prepareTasks({
-      split: N_UNITS, /* Number of tasks the job is going to be divided into */
-      width: scene.global.width,
-      height: scene.global.height
-    })
-    done()
-  })
-
-  test('ray trace', function (done) {
-    results = tasks.map(function (task) {
-      return {
-        begin_x: task.begin_x, // jscs:disable
-        end_x: task.end_x,
-        begin_y: task.begin_y,
-        end_y: task.end_y,
-        animation: task.animation,
-        data: srt.runTask(scene, task).data // jscs:enable
+  results.map(function (el) { // jscs:disable
+    let i = 0
+    for (let y = el.begin_y; y < el.end_y; y++) {
+      for (let x = el.begin_x; x < el.end_x; x++) {
+        const z = (x * scene.global.width + y) * 3
+        rgb[z] = el.data[i++]
+        rgb[z + 1] = el.data[i++]
+        rgb[z + 2] = el.data[i++]
       }
-    })
-    done()
+    } // jscs:enable
   })
+  // TODO: Learn how to do this with pngjs vs. node-png
+  // const png = new PNG(rgb, scene.global.width, scene.global.height, 'rgb')
+  // fs.writeFileSync('/tmp/out.png', png.encodeSync())
 
-  test('produce output in png', function (done) {
-    const rgb = Buffer.alloc(scene.global.width * scene.global.height * 3)
-
-    results.map(function (el) { // jscs:disable
-      let i = 0
-      for (let y = el.begin_y; y < el.end_y; y++) {
-        for (let x = el.begin_x; x < el.end_x; x++) {
-          const z = (x * scene.global.width + y) * 3
-          rgb[z] = el.data[i++]
-          rgb[z + 1] = el.data[i++]
-          rgb[z + 2] = el.data[i++]
-        }
-      } // jscs:enable
-    })
-    const png = new PNG(rgb, scene.global.width, scene.global.height, 'rgb')
-    fs.writeFileSync('/tmp/out.png', png.encodeSync())
-
-    // TODO: compare /tmp/out.png and ./expected/pokeball.png
-    done()
-  })
+  // TODO: compare /tmp/out.png and ./expected/pokeball.png
+  t.end()
 })
